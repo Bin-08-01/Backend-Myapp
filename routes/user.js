9b9eb18ccf6402f18bd256bc6c56b905fe06341b
Minh Trí <89400593+Bin-08-01@users.noreply.github.com>
2022-09-22 00:30:22 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const UserController = require("../Controllers/UserController");
 const MiddlewareController = require("../Controllers/MiddlewareController");
 const route = require('express').Router();
 
-route.use('/all', UserController.getAllUser);
+route.get('/all', UserController.getAllUser);
 
 route.delete('/delete/:id', UserController.deleteUser);
 
